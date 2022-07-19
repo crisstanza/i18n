e93d51445cd62524a155b8bd49b65626608ffb80
crisstanza <crisstanza@users.noreply.github.com>
2022-07-19 20:32:50 -0300

    JS dependencies.

---
+++ js-deps.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+updateAll() {
+	curl -o js/lib/io.github.crisstanza.Creator.js https://raw.githubusercontent.com/crisstanza/creator/main/js/io.github.crisstanza.Creator.js
+}
+
+updateAll
