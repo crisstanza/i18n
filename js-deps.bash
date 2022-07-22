3c2856d74a44021aeb63323a1776c0fe5ca229d7
crisstanza <crisstanza@users.noreply.github.com>
2022-07-22 12:52:35 -0300

    Javascript cookies library.

--- js-deps.bash
+++ js-deps.bash
@@ -2,6 +2,7 @@
 
 updateAll() {
 	curl -o js/lib/io.github.crisstanza.Creator.js https://raw.githubusercontent.com/crisstanza/creator/main/js/io.github.crisstanza.Creator.js
+	curl -o js/lib/io.github.crisstanza.Cookie-Is-Good_1.0.2.js https://raw.githubusercontent.com/crisstanza/Cookie-Is-Good/master/js/lib/io.github.crisstanza.Cookie-Is-Good_1.0.2.js
 }
 
 updateAll
