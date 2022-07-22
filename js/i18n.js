cb92f68610741f6ee74280ec7abff385e26b79a2
crisstanza <crisstanza@users.noreply.github.com>
2022-07-22 12:53:02 -0300

    Flags (links) to change the language.

--- js/i18n.js
+++ js/i18n.js
@@ -1,18 +1,17 @@
 (function () {
 
 	const keys = {
-		languages: [ 'en-US', 'pt-BR' ],
+		languages: [ 'us', 'br', 'fr', 'es' ],
 		values: {
-			'language': [ 'Language:', 'Idioma:' ]
+			'language': [ 'Language:', 'Idioma:', 'Langue:', 'Idioma:' ]
 		}
 
 	};
 
-	function init(event) {
-		io.github.crisstanza.I18N.setKeys(keys);
-		io.github.crisstanza.I18N.flags();
+	const init = (event) => {
+		io.github.crisstanza.I18N.keys(keys);
 		io.github.crisstanza.I18N.render();
-	}
+	};
 
 	window.addEventListener('load', init);
 
